872abb42f08e8155a022108d1d2dfd519a675e75
Nataliia Messner <Nataliia_Messner@epam.com>
2021-04-02 10:23:14 +0300

    alert implement

---
+++ src/components/Search.js
@@ -0,0 +1,22 @@
+import React, {useContext} from 'react';
+import {AlertContext} from '../context/alert/alertContext';
+
+export const Search = () => {
+    const {show} = useContext(AlertContext);
+    const onSubmit = (event) => {
+        if (event.key === 'Enter') {
+            show('This is alert');
+        }
+    };
+
+    return (
+        <div className='form-group'>
+            <input
+                type="text"
+                className='form-control'
+                placeholder='Enter nick of User...'
+                onKeyPress={onSubmit}
+            />
+        </div>
+    )
+};
