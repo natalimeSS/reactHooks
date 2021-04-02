8cae1932a53fc62ec1e68a796633a4cbcfd34390
Nataliia Messner <Nataliia_Messner@epam.com>
2021-04-02 15:36:31 +0300

    app is implemented

---
+++ src/components/Repos.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const Repos = ({repos}) => {
+    return (
+        <>
+            {repos.map((repo) => {
+                return (
+                    <div className='card mb-3' key={repo.id}>
+                        <div className="card-body">
+                            <h5>
+                                <a
+                                    href={repo.html_url}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                >{repo.name}</a>
+                            </h5>
+                        </div>
+                    </div>
+                )
+            })}
+        </>
+    )
+};
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
+import {GithubContext} from '../context/github/githubContext';
+import {Link} from 'react-router-dom';
+import {Repos} from '../components/Repos';
 
-export const Profile = () => {
+export const Profile = ({match}) => {
+    const {getUser, getRepos, loading, user, repos} = useContext(GithubContext);
+    const urlName = match.params.name;
+
+    useEffect(() => {
+        getUser(urlName);
+        getRepos(urlName);
+    }, []);
+
+    if (loading) {
+        return <p className='text-center'>Loading...</p>
+    }
+
+    const {
+        name, company, avatar_url,
+        location, bio, blog, login,
+        html_url, followers, public_repos,
+        public_gists, following
+    } = user;
+
     return (
-        <div>
-            <h1>Profile Page</h1>
-        </div>
+        <>
+            <Link to='/' className='btn btn-link'>To Main page</Link>
+
+            <div className="card mb-4">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-sm-3 text-center">
+                            <img
+                                src={avatar_url}
+                                alt={name}
+                                style={{width: '150px'}}
+                            />
+                            <h1>{name}</h1>
+                            {location && <p>Location: {location}</p>}
+                        </div>
+                        <div className='col'>
+                            {
+                                bio && <>
+                                    <h3>Biography</h3>
+                                    <p>{bio}</p>
+                                </>
+                            }
+                            <a
+                                href={html_url}
+                                target='_blank'
+                                rel='noreferrer'
+                                className='btn btn-dark'
+                            >Open Profile</a>
+                            <ul>
+                                {login && <li>
+                                    <strong>Username: </strong> {login}
+                                </li>}
+                                {company && <li>
+                                    <strong>Company: </strong> {company}
+                                </li>}
+                                {blog && <li>
+                                    <strong>Website: </strong> {blog}
+                                </li>}
+                            </ul>
+
+                            <div className='badge badge-primary'>Followers: {followers}</div>
+                            <div className='badge badge-success'>Follows: {following}</div>
+                            <div className='badge badge-info'>Repos: {public_repos}</div>
+                            <div className='badge badge-dark'>Gists: {public_gists}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Repos repos={repos}/>
+        </>
     )
 };
